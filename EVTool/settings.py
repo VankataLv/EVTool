fe1982e9f57a0deb3d68501de4ad1dca8fdef3aa
VankataLv <132002763+VankataLv@users.noreply.github.com>
2024-12-06 09:14:32 +0200

    Final adjustments, security key, gitignore, readme, requirements.txt

--- EVTool/settings.py
+++ EVTool/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+# from decouple import config
 from pathlib import Path
 
 from django.urls import reverse_lazy
@@ -22,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)vbqmwk^l9k+&w&)(s92&cq+jhxa3z$y20jt9wvaqkt4f@#rcw'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_fallback_secret')
+# print(SECRET_KEY)
+# SECRET_KEY = 'django-insecure-)vbqmwk^l9k+&w&)(s92&cq+jhxa3z$y20jt9wvaqkt4f@#rcw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
+# DEBUG = config('DEBUG', default=False, cast=bool)
 DEBUG = True
 
 ALLOWED_HOSTS = []
